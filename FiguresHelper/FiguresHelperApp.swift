c5029871504c3966c8daa1901016b9a16407dbda
Noah Soubliere <nsoubliere@lcs.on.ca>
2021-12-01 09:55:23 -0500

    Initial Commit

---
+++ FiguresHelper/FiguresHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiguresHelperApp.swift
+//  FiguresHelper
+//
+//  Created by Noah Alexandre Soubliere on 2021-12-01.
+//
+
+import SwiftUI
+
+@main
+struct FiguresHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
